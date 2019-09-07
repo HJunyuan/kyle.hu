d474f9bede48728f69c7c7859a05d0de12e549cc
Kyle Huang Junyuan <junyuan27@gmail.com>
2019-09-08 02:51:57 +0800

    Still finalising card layout.

--- src/components/card.js
+++ src/components/card.js
@@ -1,36 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+// import { graphql } from "gatsby";
+// import Img from "gatsby-image";
 
 import cardStyles from "./card.module.css";
-import Image from "../components/image";
 
-export default class Card extends Component {
-	static propTypes = {
-		prop: PropTypes
-	};
-
-	render() {
-		return (
-			<div className={cardStyles.cardContainer}>
-				<div className={cardStyles.cardImage}>
-					<img src="https://images.pexels.com/photos/2872767/pexels-photo-2872767.jpeg" />
-				</div>
-				<div className={cardStyles.cardDescription}>
-					<h3>Title</h3>
-					<p>
-						Something aekrjw were arer Description Description Description Description Description Description
-						Description Description{" "}
-					</p>
-				</div>
+export default function Card({ data, title, description, image }) {
+	return (
+		<div className={cardStyles.cardContainer}>
+			<div className={cardStyles.cardImage}>
+				{/* <Img fluid={} alt={this.props.title} /> */}
+				{/* <img src="https://images.pexels.com/photos/2872767/pexels-photo-2872767.jpeg" /> */}
 			</div>
-		);
-	}
+			<div className={cardStyles.cardDescription}>
+				<h3>{title}</h3>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 }
 
+// const query = graphql`
+// 	query {
+// 		placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+// 			childImageSharp {
+// 				fluid(maxWidth: 300) {
+// 					...GatsbyImageSharpFluid
+// 				}
+// 			}
+// 		}
+// 	}
+// `;
+
 Card.propTypes = {
-	imageURL: PropTypes.string
+	title: PropTypes.string,
+	description: PropTypes.string
 };
 
 Card.defaultProps = {
-	imageURL: ``
+	title: `Default Title`,
+	description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ex dui, pharetra at vehicula et, tincidunt
+	at est. Donec cursus egestas blandit.`,
+	image: `Default Image`
 };
--- src/components/layoutHome.js
+++ src/components/layoutHome.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-export default class home_layout extends Component {
+export default class HomeLayout extends Component {
 	static propTypes = {
 		prop: PropTypes
 	};
