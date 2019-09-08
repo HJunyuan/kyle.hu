15bdf71e2afcede0e800d1c7694a41a34040f17b
Kyle Huang Junyuan <junyuan27@gmail.com>
2019-09-08 20:25:02 +0800

    Wrap with Layout.

--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -2,20 +2,20 @@ import React from "react";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
 
+import Layout from "../components/layout";
+
 export default function PostTemplate({
 	data // this prop will be injected by the GraphQL query below.
 }) {
 	const { markdownRemark } = data; // data.markdownRemark holds our post data
 	const { frontmatter, html } = markdownRemark;
 	return (
-		<div className="blog-post-container">
-			<div className="blog-post">
-				<h1>{frontmatter.title}</h1>
-				<h2>{frontmatter.date}</h2>
-				<Img fluid={frontmatter.coverImg.childImageSharp.fluid} alt={frontmatter.title}></Img>
-				<div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
-			</div>
-		</div>
+		<Layout>
+			<h1>{frontmatter.title}</h1>
+			<h2>{frontmatter.date}</h2>
+			<Img fluid={frontmatter.coverImg.childImageSharp.fluid} alt={frontmatter.title}></Img>
+			<div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+		</Layout>
 	);
 }
 
@@ -29,7 +29,7 @@ export const query = graphql`
 				title
 				coverImg {
 					childImageSharp {
-						fluid(maxHeight: 300) {
+						fluid(maxHeight: 500) {
 							...GatsbyImageSharpFluid
 						}
 					}
