55f88c0688dd119f138fd1b4fd6a700c91758666
Kyle Huang Junyuan <junyuan27@gmail.com>
2019-09-08 17:49:41 +0800

    Introduced Bootstrap CSS.

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,47 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-// import { Link } from "gatsby";
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
-export default class Header extends Component {
-	static propTypes = {
-		prop: PropTypes
-	};
+import styles from "./header.module.css";
 
-	render() {
-		return <header>{this.props.siteTitle}</header>;
-	}
+export default function Header() {
+	const query = useStaticQuery(graphql`
+		{
+			placeholderImage: file(relativePath: { eq: "site-icon.png" }) {
+				childImageSharp {
+					fixed(width: 50) {
+						...GatsbyImageSharpFixed
+					}
+				}
+			}
+		}
+	`);
+
+	return (
+		<Navbar collapseOnSelect expand="lg" className={styles.navBar}>
+			<Container>
+				<Navbar.Brand href="/">
+					<Img fixed={query.placeholderImage.childImageSharp.fixed} alt="Site logo" />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link href="#about">About</Nav.Link>
+						<Nav.Link href="#contact">Contact</Nav.Link>
+						<Nav.Link href="#projects">Projects</Nav.Link>
+					</Nav>
+					<Nav>
+						<Nav.Link href="#blog">Blog</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
 
-Header.propTypes = {
-	siteTitle: PropTypes.string
-};
-
-Header.defaultProps = {
-	siteTitle: ``
-};
+// <Navbar expand="lg" variant="light" bg="light">
+// 	<Container>
+// 		<Link>{query.site.siteMetadata.title}</Link>
+// 	</Container>
+// </Navbar>
