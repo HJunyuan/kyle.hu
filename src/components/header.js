4fa8f19685c2e24d79603e8ea397f7dad0917327
Kyle Huang Junyuan <junyuan27@gmail.com>
2019-09-15 16:23:44 +0800

    Working to improve navBar CSS.

--- src/components/header.js
+++ src/components/header.js
@@ -19,19 +19,19 @@ export default function Header() {
 	`);
 
 	return (
-		<Navbar collapseOnSelect expand="lg" className={styles.navBar} variant="dark" sticky="top">
+		<Navbar collapseOnSelect expand="lg" variant="dark" sticky="top">
 			<Container>
 				<Navbar.Brand href="/">
 					<Img fixed={query.img.childImageSharp.fixed} title="Home" alt="Kyle's Logo" />
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="mr-auto">
+					<Nav>
 						<Nav.Link href="#about">About</Nav.Link>
 						<Nav.Link href="#contact">Contact</Nav.Link>
 						<Nav.Link href="#projects">Projects</Nav.Link>
 					</Nav>
-					<Nav>
+					<Nav className="ml-auto">
 						<Nav.Link href="#blog">Blog</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
