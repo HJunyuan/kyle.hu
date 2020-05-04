8b722d9daa4def1776aa16fc33a0f34ee0dace0e
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-05-04 22:04:54 +0800

    Introduced Social component

---
+++ src/components/Social.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default params => {
+  return (
+    <div>
+      {/* // Facebook
+        // Instagram
+        // GitHub
+            */}
+    </div>
+  );
+};
