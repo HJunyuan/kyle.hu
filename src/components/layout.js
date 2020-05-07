111a919c3ca3511e678cd64473dea9ca98541a75
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-05-08 02:01:30 +0800

    Set font-family for body and headings

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,10 @@ import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 
 import Navbar from "./Navbar";
+
+import "typeface-open-sans";
+import "typeface-raleway";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
