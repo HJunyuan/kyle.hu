02fe4f8723536aec34c1725d0a363b6786da8935
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-05-22 00:44:20 +0800

    Created mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,49 @@
 import React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
+import { FaHome, FaUser, FaBriefcase, FaPenAlt } from "react-icons/fa";
+import { GrArticle } from "react-icons/gr";
 
-export default ({ ...rest }) => {
+const menuItems = [
+  { label: "Home", icon: FaHome, link: "/" },
+  { label: "About", icon: FaUser, link: "/about" },
+  { label: "Projects", icon: FaBriefcase, link: "/projects" },
+  { label: "Blog", icon: FaPenAlt, link: "/blog" },
+];
+
+export default props => {
   return (
-    <Navbar {...rest}>
-      <div className="container" style={{ display: "flex" }}>
-        <Link to="/">
-          <NavbarIcon>
-            <span>K</span>
-          </NavbarIcon>
-        </Link>
-        <NavbarNav style={{ marginLeft: "auto" }}>
-          <Link to="/">
-            {/* TODO: Hide Home if in Home page */}
-            <NavbarItem>Home</NavbarItem>
+    <Navbar>
+      <NavbarNav>
+        {menuItems.map((item, i) => (
+          <Link key={i} to={item.link}>
+            <NavbarItem key={i}>
+              <NavbarIcon>
+                <item.icon />
+              </NavbarIcon>
+              <NavbarLabel>{item.label}</NavbarLabel>
+            </NavbarItem>
           </Link>
-          <Link to="/about">
-            <NavbarItem>About</NavbarItem>
-          </Link>
-          <Link to="/">
-            <NavbarItem>Projects</NavbarItem>
-          </Link>
-          <Link to="/">
-            <NavbarItem>Blog</NavbarItem>
-          </Link>
-        </NavbarNav>
-      </div>
+        ))}
+      </NavbarNav>
     </Navbar>
   );
 };
 
 const Navbar = styled.nav`
-  width: 100vw;
   position: fixed;
-  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
   margin-top: calc(var(--spacing) / 2);
   margin-bottom: calc(var(--spacing) / 2);
   z-index: 100;
 
-  a,
-  a:hover {
-    color: white;
-    text-decoration: none;
+  @media (max-width: 575.98px) {
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: 0;
+    transform: none;
   }
 `;
 
@@ -51,16 +52,44 @@ const NavbarNav = styled.ul`
   font-weight: bold;
   list-style: none;
   padding: 0;
-  margin: 0;
+  margin: 0 auto;
   border-radius: var(--navbar-height);
 
   display: flex;
   align-items: center;
+  justify-content: space-evenly;
 
   background-color: var(--color-primary);
 
-  @media screen and (max-width: 576px) {
-    display: none;
+  > a {
+    color: white;
+    text-decoration: none;
+  }
+
+  @media (min-width: 576px) {
+    > a:hover > li,
+    > a:focus > li,
+    > a:active > li {
+      color: var(--color-primary);
+      background-color: var(--color-bg);
+    }
+  }
+
+  @media (max-width: 575.98px) {
+    border-radius: 0;
+
+    > a:hover > li > span::after,
+    > a:focus > li > span::after,
+    > a:active > li > span::after {
+      --dot-size: 5px;
+      content: "";
+      display: block;
+      width: var(--dot-size);
+      height: var(--dot-size);
+      margin: 0.2rem auto 0 auto;
+      border-radius: var(--dot-size);
+      background-color: white;
+    }
   }
 `;
 
@@ -70,33 +99,51 @@ const NavbarItem = styled.li`
   border-radius: var(--navbar-height);
 
   transition: background-color 0.5s cubic-bezier(0.16, 1, 0.3, 1);
-
-  :hover {
-    color: var(--color-primary);
-    background-color: var(--color-bg);
-  }
 `;
 
-const NavbarIcon = styled.div`
-  width: var(--navbar-height);
-  height: var(--navbar-height);
-  border-radius: var(--navbar-height);
-  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
-    "Lucida Sans", Arial, sans-serif;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
+const NavbarIcon = styled.span`
   color: white;
-  background-color: var(--color-primary);
+  font-size: 1.7rem;
+  line-height: 1rem;
 
-  span {
-    transform: scale(1);
-    transition: transform 0.3s cubic-bezier(0.16, 1, 0.3, 1);
-  }
-
-  :hover span {
-    transform: scale(2);
+  @media (min-width: 576px) {
+    display: none;
   }
 `;
+
+const NavbarLabel = styled.span`
+  color: inherit;
+
+  @media (max-width: 575.98px) {
+    display: none;
+  }
+`;
+
+/* <Link to="/">
+  <NavbarIcon>
+    <span>K</span>
+  </NavbarIcon>
+</Link> */
+// const NavbarIcon = styled.div`
+//   width: var(--navbar-height);
+//   height: var(--navbar-height);
+//   border-radius: var(--navbar-height);
+//   font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+//     "Lucida Sans", Arial, sans-serif;
+
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+
+//   color: white;
+//   background-color: var(--color-primary);
+
+//   span {
+//     transform: scale(1);
+//     transition: transform 0.3s cubic-bezier(0.16, 1, 0.3, 1);
+//   }
+
+//   :hover span {
+//     transform: scale(2);
+//   }
+// `;
