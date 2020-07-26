0f3dd801e8110d81d3a633adbf2be6665e9a15cb
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-07-26 12:53:57 +0800

    Init TypeScript

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,32 @@
  */
 
 // You can delete this file if you're not using it
+// TODO: Auto generate blog and project lists
+// const path = require("path");
+
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   const blogPage = path.resolve("src/templates/blog-list.jsx");
+//   const projectPage = path.resolve("src/templates/project-list.jsx");
+
+//   const markdownQueryResult = await graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             frontmatter {
+//               title
+//               tags
+//               date
+//             }
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `);
+
+//   console.log("QueryResult: ", markdownQueryResult);
+// };
